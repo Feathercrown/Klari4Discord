4baf05ef9c063aaf66eef591993e7e7ec6893933
Klari Lunan <crystaltheandroid@gmail.com>
2017-02-21 18:10:59 -0500

    Update 2/21/17
    Added roleplay.js

---
+++ shell_commands/roleplay.js
@@ -0,0 +1,3 @@
+exports.run = (bot, message, args) => {
+    message.channel.sendMessage(`*This command is still being worked on. I'll play make-believe any time!*`);
+}
--- shell_commands/rps.js
+++ shell_commands/rps.js
@@ -1,3 +1,3 @@
 exports.run = (bot, message, args) => {
-    message.channel.sendMessage(`*This command is still being worked on. Think you can beat me at a game?*`);
+    message.channel.sendMessage(`*This command is still being worked on. Think you can beat me at a game of Rock, Paper, Scissors?*`);
 }
