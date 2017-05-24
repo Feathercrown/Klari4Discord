5aa94b45c4e9f8f02db47636fe3a9b635347c82c
Feathercrown <gabriel.buonomano@gmail.com>
2017-05-24 19:26:09 -0400

    Update rps.js

--- shell_commands/rps.js
+++ shell_commands/rps.js
@@ -1,7 +1,11 @@
 exports.run = (bot, message, args) => {
+    if(args.length>0&&args[0]<1){
+       return message.channel.sendMessage(`*We can't have a Best of "${args[0]}" game!*`);
+    }
     var bestOf=args.length==0?"Best of 1":"Best of "+args[0];
     message.channel.sendMessage(`*${bestOf} Rock, Paper, Scissors game started.
 Simply say Rock, Paper, or Scissors to start a new round.*`);
     var rpsRound=1;
+    var score=0;
     mode="rps";
 }
