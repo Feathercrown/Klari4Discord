ff700f6a32c6e75df9f979384e3e2710f1f59c6b
Feathercrown <gabriel.buonomano@gmail.com>
2017-05-24 20:00:16 -0400

    Update k4d.js

--- k4d.js
+++ k4d.js
@@ -1,10 +1,11 @@
+/*jshint esversion: 6 */
 console.log('');
 console.log('Loading Colors...');
 const Colors =  require('colors');
 
 console.log('Loading Settings.json...');
 const config = require('./settings.json')
-
+;
 console.log('Loading Discord.js Core...');
 const Discord = require('discord.js');
 
@@ -14,7 +15,7 @@ const fs = require('fs');
 console.log('Creating new Discord.Client()...');
 const bot = new Discord.Client();
 
-console.log('Initializing Modes...')
+console.log('Initializing Modes...');
 var mode="commands";
 
 console.log('Starting in Terminal Mode...');
@@ -27,19 +28,21 @@ bot.on('ready', () => {
 ┃   ╲   ┃ ┃         ╱ ╱       ╲ ╲   ┃ ┃       ┃ ┃
 ┃ ┃╲ ╲  ┃ ┃        ╱ ╱         ╲ ╲  ┃ ┃       ┃ ┃
 ┃ ┃ ╲ ╲ ┃ ┗━━━━━┓ ╱ ╱           ╲ ╲ ┃ ┃       ┃ ┃
-‾‾‾  ‾‾ ‾‾‾‾‾‾‾‾‾ ‾‾             ‾‾ ‾‾‾       ‾‾‾`.magenta)
+‾‾‾  ‾‾ ‾‾‾‾‾‾‾‾‾ ‾‾             ‾‾ ‾‾‾       ‾‾‾`.magenta);
     console.log("Generation D0");
     console.log("");
     console.log("All interactions with this bot take place on the Discord server.");
     console.log("Ctrl-C to shutdown.");
-    bot.channels.get('264593288219197440').send(`*Klari-bot is Online. Starting in mode: ${mode}.*`)
+    bot.channels.get('264593288219197440').send(`*Klari-bot is Online. Starting in mode: ${mode}.*`);
 });
 
 bot.on('message', message => {
     if(mode=="commands"){
-        if(!message.content.startsWith(config.prefix)) return;
+  		if(!message.content.startsWith(config.prefix)){
+ 			  return;
+			}
 
-        var command = message.content.split(" ")[0];
+      	var command = message.content.split(" ")[0];
         command = command.slice(config.prefix.length);
         if(!command) return message.channel.sendMessage(emptyreply());
         outerbreak: {
@@ -58,30 +61,7 @@ bot.on('message', message => {
             console.error(err);
         }
 
-        function emptyreply(){
-            let replies = [
-                `*Huh?*`,
-                `*What?*`,
-                `*Yes, love?*`,
-                `*Did you need me for something, ${message.author.username}?*`,
-                `*What is it that I may help you with today, ${message.author.username}?*`,
-                `*${message.author.username}, did you mean to type something?*`,
-                `*How may I be of assistance, ${message.author.username}*?`,
-                `*${message.author.username}, if you don't know what to type, I suggest entering 'kshell>help' to give you a few ideas. Happy Coding!*`
-            ];
-            return replies[Math.floor(Math.random()*replies.length)];
-        }
-        function notmatching(){
-               let replies = [
-               `*Excuse me?*`,
-               `*What?*`,
-               `*Uhh, I don't know that yet. You should suggest it in the 'klaribot-suggestions' channel if you have any ideas!*`,
-              `*Did you mean something else?*`,
-               `*I'm confused...*`,
-                `*Silly ${message.author.username}! You should go look at 'kshell>help'!*`,
-            ];
-            return replies[Math.floor(Math.random()*replies.length)];
-        } else if(mode=="rps"){
+} else if(mode=="rps"){
             var msg = message.content.toLowerCase();
             if(msg=="r"){
                 msg="rock";
@@ -113,6 +93,32 @@ ${whoWon}*`);
             }
         }
     }
-});
+);
+
+
+        function emptyreply(){
+            let replies = [
+                `*Huh?*`,
+                `*What?*`,
+                `*Yes, love?*`,
+                `*Did you need me for something, ${message.author.username}?*`,
+                `*What is it that I may help you with today, ${message.author.username}?*`,
+                `*${message.author.username}, did you mean to type something?*`,
+                `*How may I be of assistance, ${message.author.username}*?`,
+                `*${message.author.username}, if you don't know what to type, I suggest entering 'kshell>help' to give you a few ideas. Happy Coding!*`
+            ];
+            return replies[Math.floor(Math.random()*replies.length)];
+        }
+        function notmatching(){
+               let replies = [
+               `*Excuse me?*`,
+               `*What?*`,
+               `*Uhh, I don't know that yet. You should suggest it in the 'klaribot-suggestions' channel if you have any ideas!*`,
+              `*Did you mean something else?*`,
+               `*I'm confused...*`,
+                `*Silly ${message.author.username}! You should go look at 'kshell>help'!*`,
+            ];
+            return replies[Math.floor(Math.random()*replies.length)];
+        }
 
 bot.login(config.token);
